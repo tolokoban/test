76b7d9a6aa532df6a934b4fb3fe0fc188d8e35ac
Tolokoban <contact@tolokoban.org>
2019-07-14 12:43:42 +0200

    Updates

---
+++ precache-manifest.69d044a2fe5d96c46edf2cdb10a6266c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "61b57ae859b013329bab",
+    "url": "/static/js/main.0e3ca0a7.chunk.js"
+  },
+  {
+    "revision": "b39304ed4c49f6173cb3",
+    "url": "/static/js/2.feb7fc16.chunk.js"
+  },
+  {
+    "revision": "61b57ae859b013329bab",
+    "url": "/static/css/main.6b0cd3c1.chunk.css"
+  },
+  {
+    "revision": "6e2a941c6c33bf6efff8846bb7e47c4f",
+    "url": "/index.html"
+  }
+];
